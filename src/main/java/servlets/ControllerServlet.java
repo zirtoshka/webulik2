7e3a1b92f44f14d8ffc721519e1e9f1f8617a774
zirtoshka <367222@edu.itmo.ru>
2023-11-13 00:25:43 +0300

    ввв

--- src/main/java/servlets/ControllerServlet.java
+++ src/main/java/servlets/ControllerServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Controller", value="/app", urlPatterns = "/app")
+@WebServlet(name = "Controller", value="/app")
 public class ControllerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
